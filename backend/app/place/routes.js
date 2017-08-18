4f746a489ddec4fe3390246f325db28915eff25c
kashifahmed91 <kashif91ahmed@gmail.com>
2017-08-18 22:22:40 +0500

    add image upload functionality

--- backend/app/place/routes.js
+++ backend/app/place/routes.js
@@ -1,4 +1,5 @@
 'use strict';
+var path = require('path');
 
 module.exports = function(app, router, scope) {
     router.route('/place/:id?')
@@ -10,5 +11,26 @@ module.exports = function(app, router, scope) {
 
     router.route('/favorite/:id?')
         .post(app.services._authorization, scope.controllers._markFavoriteItem)
-        .get(app.services._authorization, scope.controllers._getFavoriteItem)
+        .get(app.services._authorization, scope.controllers._getFavoriteItem);
+
+
+    router.route('/file/upload').post(function(req, res) {
+        if (!req.files) {
+            res.send({
+                "message": "file Not found"
+            });
+        } else {
+            let sampleFile = req.files.sampleFile;
+            console.log(req.files.sampleFile);
+            var fileName = new Date();
+            var Url = path.resolve(__dirname, '../../public/uploads/' + fileName.getTime() + '.jpg');
+            // Use the mv() method to place the file somewhere on your server 
+            sampleFile.mv(Url, function(err) {
+                if (err)
+                    return res.status(500).send(err);
+
+                res.send('File uploaded!');
+            });
+        }
+    });
 };
